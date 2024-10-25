615f857fa3c5723dc35a9b227814e04a3d3e262a
Vishal Haramkar <160427514+VishalH01@users.noreply.github.com>
2024-10-25 19:27:21 +0530

    Add files via upload

---
+++ teleporthq/src/components/hero.js
@@ -0,0 +1,264 @@
+import React from 'react'
+
+import Script from 'dangerous-html/react'
+import PropTypes from 'prop-types'
+
+import './hero.css'
+
+const Hero = (props) => {
+  return (
+    <div className="hero-header78">
+      <div className="hero-column thq-section-padding thq-section-max-width">
+        <div className="hero-content1">
+          <h1 className="hero-text1 thq-heading-1">{props.heading1}</h1>
+          <p className="hero-text2 thq-body-large">{props.content1}</p>
+        </div>
+        <div className="hero-actions">
+          <button className="thq-button-filled hero-button1">
+            <span className="thq-body-small">{props.action1}</span>
+          </button>
+          <button className="thq-button-outline hero-button2">
+            <span className="thq-body-small">{props.action2}</span>
+          </button>
+        </div>
+      </div>
+      <div className="hero-content2">
+        <div className="hero-row-container1 thq-animated-group-container-horizontal thq-mask-image-horizontal">
+          <div className="thq-animated-group-horizontal">
+            <img
+              alt={props.image1Alt}
+              src={props.image1Src}
+              className="hero-placeholder-image10 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image2Alt}
+              src={props.image2Src}
+              className="hero-placeholder-image11 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image3Alt}
+              src={props.image3Src}
+              className="hero-placeholder-image12 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image4Alt}
+              src={props.image4Src}
+              className="hero-placeholder-image13 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image5Alt}
+              src={props.image5Src}
+              className="hero-placeholder-image14 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image6Alt}
+              src={props.image6Src}
+              className="hero-placeholder-image15 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+          <div className="thq-animated-group-horizontal">
+            <img
+              alt={props.image1Alt}
+              src={props.image1Src}
+              className="hero-placeholder-image16 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image2Alt}
+              src={props.image2Src}
+              className="hero-placeholder-image17 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image3Alt}
+              src={props.image3Src}
+              className="hero-placeholder-image18 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image4Alt}
+              src={props.image4Src}
+              className="hero-placeholder-image19 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image5Alt}
+              src={props.image5Src}
+              className="hero-placeholder-image20 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt="Hero Image"
+              src="https://images.unsplash.com/photo-1534312527009-56c7016453e6?ixid=M3w5MTMyMXwwfDF8c2VhcmNofDIxfHxhYnN0cmFjdHxlbnwwfHx8fDE3MTA4NzA5MzB8MA&amp;ixlib=rb-4.0.3&amp;w=1500"
+              className="hero-placeholder-image21 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+        </div>
+        <div className="hero-row-container2 thq-animated-group-container-horizontal thq-mask-image-horizontal">
+          <div className="thq-animated-group-horizontal-reverse">
+            <img
+              alt={props.image7Alt}
+              src={props.image7Src}
+              className="hero-placeholder-image22 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image8Alt}
+              src={props.image8Src}
+              className="hero-placeholder-image23 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image9Alt}
+              src={props.image9Src}
+              className="hero-placeholder-image24 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image10Alt}
+              src={props.image10Src}
+              className="hero-placeholder-image25 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image11Alt}
+              src={props.image11Src}
+              className="hero-placeholder-image26 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image12Alt}
+              src={props.image12Src}
+              className="hero-placeholder-image27 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+          <div className="thq-animated-group-horizontal-reverse">
+            <img
+              alt={props.image7Alt}
+              src={props.image7Src}
+              className="hero-placeholder-image28 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image8Alt}
+              src={props.image8Src}
+              className="hero-placeholder-image29 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image9Alt}
+              src={props.image9Src}
+              className="hero-placeholder-image30 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image10Alt}
+              src={props.image10Src}
+              className="hero-placeholder-image31 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt={props.image11Alt}
+              src={props.image11Src}
+              className="hero-placeholder-image32 thq-img-scale thq-img-ratio-1-1"
+            />
+            <img
+              alt="Hero Image"
+              src="https://images.unsplash.com/photo-1568214379698-8aeb8c6c6ac8?ixid=M3w5MTMyMXwwfDF8c2VhcmNofDEyfHxncmFmaWN8ZW58MHx8fHwxNzE1Nzk0OTk5fDA&amp;ixlib=rb-4.0.3&amp;w=1500"
+              className="hero-placeholder-image33 thq-img-scale thq-img-ratio-1-1"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="hero-container2">
+          <Script
+            html={`<style>
+  @keyframes scroll-x {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(calc(-100% - 16px));
+    }
+  }
+
+  @keyframes scroll-y {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(calc(-100% - 16px));
+    }
+  }
+</style>
+`}
+          ></Script>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Hero.defaultProps = {
+  image3Src:
+    'https://images.unsplash.com/photo-1541272050316-4f64ff810abc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4M3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image8Alt: 'Hero Image',
+  image2Src:
+    'https://images.unsplash.com/photo-1532578498858-e21a39e0a449?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4NHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image6Alt: 'Hero Image',
+  image11Src:
+    'https://images.unsplash.com/photo-1676288176877-ee3678ab1fea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4NXw&ixlib=rb-4.0.3&q=80&w=1080',
+  image5Alt: 'Hero Image',
+  image1Alt: 'Car Rental Service',
+  image7Src:
+    'https://images.unsplash.com/photo-1606224103857-e4c74b0139df?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image7Alt: 'Hero Image',
+  image12Alt: 'Hero Image',
+  image2Alt: 'Hero Image',
+  image6Src:
+    'https://images.unsplash.com/photo-1615125468484-088e3dfcabb6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image12Src:
+    'https://images.unsplash.com/photo-1590362891991-f776e747a588?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image3Alt: 'Hero Image',
+  image9Src:
+    'https://images.unsplash.com/photo-1554294314-80a5fb7e6bd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4Nnw&ixlib=rb-4.0.3&q=80&w=1080',
+  image11Alt: 'Hero Image',
+  action2: 'Learn More',
+  action1: 'Book Now',
+  image8Src:
+    'https://images.unsplash.com/photo-1468818438311-4bab781ab9b8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image5Src:
+    'https://images.unsplash.com/photo-1618884257940-6e8d53a0753a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image4Src:
+    'https://images.unsplash.com/photo-1597849428022-92605a5e5309?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4N3w&ixlib=rb-4.0.3&q=80&w=1080',
+  image10Alt: 'Hero Image',
+  image4Alt: 'Hero Image',
+  heading1: 'Explore the World with Our Car Rental Service',
+  content1:
+    "Get ready for your next adventure with our reliable and affordable car rental service. Whether you're planning a road trip, a business trip, or just need a temporary ride, we've got you covered.",
+  image10Src:
+    'https://images.unsplash.com/photo-1547245324-d777c6f05e80?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4OHw&ixlib=rb-4.0.3&q=80&w=1080',
+  image9Alt: 'Hero Image',
+  image1Src:
+    'https://images.unsplash.com/photo-1491921125492-f0b9c835b699?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyNDc3ODM4OHw&ixlib=rb-4.0.3&q=80&w=1080',
+}
+
+Hero.propTypes = {
+  image3Src: PropTypes.string,
+  image8Alt: PropTypes.string,
+  image2Src: PropTypes.string,
+  image6Alt: PropTypes.string,
+  image11Src: PropTypes.string,
+  image5Alt: PropTypes.string,
+  image1Alt: PropTypes.string,
+  image7Src: PropTypes.string,
+  image7Alt: PropTypes.string,
+  image12Alt: PropTypes.string,
+  image2Alt: PropTypes.string,
+  image6Src: PropTypes.string,
+  image12Src: PropTypes.string,
+  image3Alt: PropTypes.string,
+  image9Src: PropTypes.string,
+  image11Alt: PropTypes.string,
+  action2: PropTypes.string,
+  action1: PropTypes.string,
+  image8Src: PropTypes.string,
+  image5Src: PropTypes.string,
+  image4Src: PropTypes.string,
+  image10Alt: PropTypes.string,
+  image4Alt: PropTypes.string,
+  heading1: PropTypes.string,
+  content1: PropTypes.string,
+  image10Src: PropTypes.string,
+  image9Alt: PropTypes.string,
+  image1Src: PropTypes.string,
+}
+
+export default Hero
